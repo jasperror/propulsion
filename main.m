48622f3afdaf0ffb94424c6930b8b703c48a8d96
Jasper <32520678+jasperror@users.noreply.github.com>
2026-02-26 08:53:20 -0800

    Added basic structure

--- main.m
+++ main.m
@@ -1,1 +1,61 @@
-% Hello
+% ENGR477 Propulsion
+% Project 1: F135 Engine
+% Jasper Palmer & Jackson Gilbert
+
+clear; close all; clc;
+
+%% Engine Components
+
+function out = diffuser()
+
+end
+
+function out = nozzle()
+
+end
+
+function out = fan()
+
+end
+
+function out = compressor()
+
+end
+
+function out = turbine()
+
+end
+
+function out = combustor()
+
+end
+
+function out = duct()
+
+end
+
+function out = mixer()
+
+end
+
+function out = afterburner()
+
+end
+
+%% Engine Structure
+
+% Element Numbering
+%{
+a: Atmospheric (inlet condition)
+1: Fan inlet
+2: Fan outlet
+3: LPC outlet
+4: HPC outlet
+5: Combustor outlet
+6: HPT outlet
+7: Bypass outlet
+7': LPT outlet
+8: Mixer outlet
+9: Afterburner outlet
+10: Nozzle outlet
+%}
